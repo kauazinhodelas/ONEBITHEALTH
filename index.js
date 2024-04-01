ff35c360ecce4461ecaad0830147ee948d76bd87
kauazinhodelas <santoskaua058@gmail.com>
2024-04-01 12:02:46 -0300

    PRIMEIRO COMIT DO 3 B3

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View } from "react-native";
+
+export default function Title(){
+    return(
+        <View>
+
+            <Text>ONEBITHEALTH</Text>
+            <Text>01/04/2024 3B3 Lado A</Text>
+
+
+        </View>
+
+    );
+
+}
