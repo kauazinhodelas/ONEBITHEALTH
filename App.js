f898add164184989bb4d381bb4a9425337f87ddf
kauazinhodelas <santoskaua058@gmail.com>
2024-05-20 12:04:44 -0300

    criar componente resultImc

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './SRC/components/Title';
+import Main from './SRC/components/Main';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      
+     
+      <Title/>
+      <Main/>
     </View>
   );
 }
